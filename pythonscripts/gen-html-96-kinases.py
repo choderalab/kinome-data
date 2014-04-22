f3e7c3786402afd7569a717ffe905ade19c226fc
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-22 16:12:31 -0400

    Added html table and code for selection of 96 kinases from SGC and HIP plasmid libraries.

---
+++ pythonscripts/gen-html-96-kinases.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import lxml.html
+from lxml import etree
+from lxml.html import builder as E
+import StringIO
+import jinja2
+
+title = '96 kinases'
+subtitle = 'Selected from SGC and HIP pJP1520 plasmid libraries'
+html_output_cols = ['cloneID', 'UniProt_entry_name', 'nconflicts_target_domain_region', 'nextraneous_plasmid_residues', 'DB_target_rank']
+
+parser = etree.HTMLParser(remove_blank_text=True)
+
+df = pd.DataFrame.from_csv('../PDB_construct_selection/96-kinases-sgc_and_hip.csv')
+
+pd_html = df.to_html(columns=html_output_cols)
+html = etree.parse(StringIO.StringIO(pd_html), parser).getroot()
+table_children = html.find('body/table').getchildren()
+table_content = '\n'.join([etree.tostring(element) for element in table_children])
+
+env = jinja2.Environment(loader=jinja2.PackageLoader('app', 'templates'))
+template = env.get_template('expression-construct-table.html')
+
+with open('96-kinases-sgc_and_hip.html', 'w') as html_file:
+    html_file.write( template.render(title=title, subtitle=subtitle, maintable=table_content) )
+
