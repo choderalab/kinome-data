b5513f657aa1044db92e86649cf7bc215efb023b
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-22 17:42:30 -0400

    Added SGC Oxford alignment files.

--- pythonscripts/gen-html-96-kinases.py
+++ pythonscripts/gen-html-96-kinases.py
@@ -35,6 +35,13 @@ for tr in tbody:
         aelem = etree.SubElement(cloneID_td, 'a')
         aelem.set('href', 'plasmid-construct-alignments/HIP-pJP1520/' + targetID + '.html')
         aelem.text = cloneID
+    if 'SGC Oxford' in tr.findtext('td'):
+        cloneID_td = tr.find('td[2]')
+        cloneID = cloneID_td.text
+        cloneID_td.text = ''
+        aelem = etree.SubElement(cloneID_td, 'a')
+        aelem.set('href', 'plasmid-construct-alignments/SGC-Oxford/' + cloneID.strip() + '.html')
+        aelem.text = cloneID
 
 # ========
 # Render html from template and output
@@ -54,10 +61,17 @@ with open('96-kinases-sgc_and_hip.html', 'w') as html_file:
 
 # copy CSS
 shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/HIP-pJP1520/seqlib.css')
+shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/SGC-Oxford/seqlib.css')
 
 HIP_plasmid_targetIDs = list( df[df['plasmid_lib'] == 'HIP pJP1520']['targetID'] )
 for targetID in HIP_plasmid_targetIDs:
     src_html_filepath = os.path.join('..', 'PDB_construct_selection', 'alignments', targetID + '.html') # XXX directory location will prob change soon
     dest_html_filepath = os.path.join('plasmid-construct-alignments', 'HIP-pJP1520', targetID + '.html')
+    shutil.copy(src_html_filepath, dest_html_filepath)
+
+SGC_plasmid_cloneIDs = list( df[df['plasmid_lib'] == 'SGC Oxford']['cloneID'] )
+for cloneID in SGC_plasmid_cloneIDs:
+    src_html_filepath = os.path.join('..', 'plasmids', 'SGC', 'Oxford_SGC_Clones', 'alignments', cloneID + '.html')
+    dest_html_filepath = os.path.join('plasmid-construct-alignments', 'SGC-Oxford', cloneID + '.html')
     shutil.copy(src_html_filepath, dest_html_filepath)
 
