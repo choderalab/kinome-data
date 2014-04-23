d4fdf630bce5e7654f66221c776f3c647816cc9f
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-23 16:57:57 -0400

    Added PDB construct data to SGC plasmids in 96 kinase selection.

--- PDB_construct_selection/scripts/select-96-kinase-constructs.py
+++ PDB_construct_selection/scripts/select-96-kinase-constructs.py
@@ -11,14 +11,14 @@ print 'Number of SGC plasmids with < 10 conflicts in the target domain region:',
 selected_sgc_plasmids.sort('DB_target_rank', inplace=True)
 selected_sgc_plasmids.reset_index(inplace=True) # add numerical index and move 'cloneID' to a column
 
-pdb_constructs = pd.DataFrame.from_csv('PDB_constructs-data.csv')
+all_pdb_constructs = pd.DataFrame.from_csv('PDB_constructs-data.csv')
 
 
 # ========
 # Filter out PDB constructs with no matching UniProtAC, and those with an authenticity score <= 0
 # ========
-pdb_constructs_with_matching_UniProtAC = pdb_constructs['UniProtAC'].notnull()
-pdb_constructs = pdb_constructs[ pdb_constructs_with_matching_UniProtAC ]
+pdb_constructs_with_matching_UniProtAC = all_pdb_constructs['UniProtAC'].notnull()
+pdb_constructs = all_pdb_constructs[ pdb_constructs_with_matching_UniProtAC ]
 
 pdb_constructs_with_positive_auth_score = pdb_constructs['top_cnstrct_auth_score'] > 0
 pdb_constructs = pdb_constructs[ pdb_constructs_with_positive_auth_score ]
@@ -41,6 +41,7 @@ selected_pdb_constructs = selected_pdb_constructs.head(nselected_pdb_constructs)
 selected_pdb_constructs.reset_index(inplace=True)
 selected_pdb_constructs.set_index(np.arange(len(selected_sgc_plasmids), 96), inplace=True)
 
+
 # ========
 # Rename columns to be more readable, and so that sgc and pdb column names match
 # ========
@@ -48,15 +49,53 @@ pdb_rename_dict = {
 'top_cloneID': 'cloneID',
 'top_plasmid_nconflicts': 'nconflicts_target_domain_region',
 'nmatching_PDB_structures':'nPDBs',
-'top_cnstrct_expr_tag':'expr_tag',
-'top_cnstrct_auth_score':'auth_score',
+'top_cnstrct_expr_tag':'top_expr_tag',
+'top_cnstrct_auth_score':'top_auth_score',
 }
+all_pdb_constructs.rename(columns=pdb_rename_dict, inplace=True)
+pdb_constructs.rename(columns=pdb_rename_dict, inplace=True)
 selected_pdb_constructs.rename(columns=pdb_rename_dict, inplace=True)
 
 sgc_rename_dict = {
 'matching_targetID': 'targetID',
 }
 selected_sgc_plasmids.rename(columns=sgc_rename_dict, inplace=True)
+
+
+# ========
+# For targets with multiple SGC plasmids, rank these and keep only the top-ranked plasmid
+# ========
+
+clones_to_delete = []
+
+for targetID in set(selected_sgc_plasmids['targetID']):
+    target_plasmids = selected_sgc_plasmids[ selected_sgc_plasmids['targetID'] == targetID ]
+    if len(target_plasmids) > 1:
+        print '%d SGC plasmids found for targetID %s. Will rank and keep only the top-ranked plasmid.' % (len(target_plasmids), targetID)
+        target_plasmids.sort(('nextraneous_plasmid_residues', 'nconflicts_target_domain_region'), inplace=True)
+        clones_to_delete += [cloneID for cloneID in target_plasmids['cloneID'] ][1:]
+
+clones_to_keep = [ False if cloneID in clones_to_delete else True for cloneID in selected_sgc_plasmids['cloneID'] ]
+selected_sgc_plasmids = selected_sgc_plasmids[ clones_to_keep ]
+
+
+# ========
+# Add ExpressionExplorer results for SGC plasmids
+# ========
+
+sgc_additional_data = {'nPDBs':[], 'top_expr_tag':[], 'top_auth_score':[]}
+for targetID in selected_sgc_plasmids['targetID']:
+    pdb_constructs_data = all_pdb_constructs[all_pdb_constructs['targetID'] == targetID]
+    nPDBs = pdb_constructs_data['nPDBs'].values[0]
+    expr_tag = pdb_constructs_data['top_expr_tag'].values[0]
+    auth_score = pdb_constructs_data['top_auth_score'].values[0]
+    sgc_additional_data['nPDBs'].append( nPDBs )
+    sgc_additional_data['top_expr_tag'].append( expr_tag )
+    sgc_additional_data['top_auth_score'].append( auth_score )
+
+for key in sgc_additional_data:
+    selected_sgc_plasmids[key] = pd.Series(sgc_additional_data[key], index=selected_sgc_plasmids.index)
+
 
 # ========
 # Add column defining plasmid library source
