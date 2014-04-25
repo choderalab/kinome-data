a2581328563ff32ce8510cec64f7041578f3847f
Daniel L. Parton <Danny.Parton@gmail.com>
2014-04-25 00:28:18 -0400

    Updates to 96 kinase construct selection.

--- pythonscripts/gen-html-96-kinases.py
+++ pythonscripts/gen-html-96-kinases.py
@@ -11,11 +11,11 @@ import jinja2
 
 title = '96 kinases'
 subtitle = 'Selected from SGC Oxford and HIP pJP1520 plasmid libraries'
-html_output_cols = ['plasmid_lib', 'cloneID', 'targetID', 'nconflicts_target_domain_region', 'nextraneous_plasmid_residues', 'nPDBs', 'top_expr_tag', 'top_auth_score', 'DB_target_rank']
+html_output_cols = ['targetID', 'DB_target_rank', 'plasmid_source', 'plasmid_ID', 'plasmid_nconflicts_in_domain', 'plasmid_nextraneous_residues', 'nPDBs', 'top_pdb_ID', 'top_pdb_expr_tag', 'top_pdb_auth_score', 'top_pdb_nextraneous_residues', 'top_pdb_taxon']
 
 parser = etree.HTMLParser(remove_blank_text=True)
 
-df = pd.DataFrame.from_csv('../PDB_construct_selection/96-kinases-sgc_and_hip.csv')
+df = pd.DataFrame.from_csv('../kinase-expression-constructs/96-kinases-sgc_and_hip.csv')
 
 pd_html = df.to_html(columns=html_output_cols)
 html = etree.parse(StringIO.StringIO(pd_html), parser).getroot()
@@ -34,7 +34,7 @@ for tr in thead:
             element.text = element.text.strip()
         except AttributeError:
             continue
-        if element.text in ['nconflicts_target_domain_region', 'nextraneous_plasmid_residues', 'nPDBs', 'auth_score', 'DB_target_rank']:
+        if element.text in ['plasmid_nconflicts_in_domain', 'plasmid_nextraneous_residues', 'nPDBs', 'top_pdb_auth_score', 'top_pdb_nextraneous_residues', 'DB_target_rank']:
             element.set("class", "{sorter: 'digit'}")
 for tr in tbody:
     for element in tr.getchildren():
@@ -45,21 +45,24 @@ for tr in tbody:
 # ========
 
 for tr in tbody:
-    if 'HIP pJP1520' in tr.findtext('td'):
-        cloneID_td = tr.find('td[2]')
-        targetID = tr.findtext('td[3]').strip()
-        cloneID = cloneID_td.text
-        cloneID_td.text = ''
-        aelem = etree.SubElement(cloneID_td, 'a')
-        aelem.set('href', 'plasmid-construct-alignments/HIP-pJP1520/' + targetID + '.html')
-        aelem.text = cloneID
-    if 'SGC Oxford' in tr.findtext('td'):
-        cloneID_td = tr.find('td[2]')
-        cloneID = cloneID_td.text
-        cloneID_td.text = ''
-        aelem = etree.SubElement(cloneID_td, 'a')
-        aelem.set('href', 'plasmid-construct-alignments/SGC-Oxford/' + cloneID.strip() + '.html')
-        aelem.text = cloneID
+    for td in tr:
+        if 'HIP pJP1520' in td.text:
+            #cloneID_td = tr.find('td[4]')
+            targetID_td = tr.find('td[1]')
+            targetID = targetID_td.text
+            targetID_td.text = ''
+            aelem = etree.SubElement(targetID_td, 'a')
+            aelem.set('href', 'plasmid-construct-alignments/' + targetID + '.html')
+            aelem.text = targetID
+            break
+        elif 'SGC Oxford' in td.text:
+            targetID_td = tr.find('td[1]')
+            targetID = targetID_td.text
+            targetID_td.text = ''
+            aelem = etree.SubElement(targetID_td, 'a')
+            aelem.set('href', 'plasmid-construct-alignments/' + targetID + '.html')
+            aelem.text = targetID
+            break
 
 # ========
 # Render html from template and output
@@ -78,18 +81,19 @@ with open('96-kinases-sgc_and_hip.html', 'w') as html_file:
 # ========
 
 # copy CSS
-shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/HIP-pJP1520/seqlib.css')
-shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/SGC-Oxford/seqlib.css')
+# shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/HIP-pJP1520/seqlib.css')
+# shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/SGC-Oxford/seqlib.css')
+shutil.copy('stylesheets/seqlib.css', 'plasmid-construct-alignments/seqlib.css')
 
-HIP_plasmid_targetIDs = list( df[df['plasmid_lib'] == 'HIP pJP1520']['targetID'] )
-for targetID in HIP_plasmid_targetIDs:
-    src_html_filepath = os.path.join('..', 'PDB_construct_selection', 'alignments', targetID + '.html') # XXX directory location will prob change soon
-    dest_html_filepath = os.path.join('plasmid-construct-alignments', 'HIP-pJP1520', targetID + '.html')
+targetIDs = list(df['targetID'])
+for targetID in targetIDs:
+    src_html_filepath = os.path.join('..', 'kinase-expression-constructs', 'alignments', targetID + '.html') # XXX directory location will prob change soon
+    dest_html_filepath = os.path.join('plasmid-construct-alignments', targetID + '.html')
     shutil.copy(src_html_filepath, dest_html_filepath)
 
-SGC_plasmid_cloneIDs = list( df[df['plasmid_lib'] == 'SGC Oxford']['cloneID'] )
-for cloneID in SGC_plasmid_cloneIDs:
-    src_html_filepath = os.path.join('..', 'plasmids', 'SGC', 'Oxford_SGC_Clones', 'alignments', cloneID + '.html')
-    dest_html_filepath = os.path.join('plasmid-construct-alignments', 'SGC-Oxford', cloneID + '.html')
-    shutil.copy(src_html_filepath, dest_html_filepath)
+# SGC_plasmid_cloneIDs = list( df[df['plasmid_source'] == 'SGC Oxford']['cloneID'] )
+# for cloneID in SGC_plasmid_cloneIDs:
+#     src_html_filepath = os.path.join('..', 'kinase-expression-constructs', 'alignments', targetID + '.html')
+#     dest_html_filepath = os.path.join('plasmid-construct-alignments', 'SGC-Oxford', cloneID + '.html')
+#     shutil.copy(src_html_filepath, dest_html_filepath)
 
